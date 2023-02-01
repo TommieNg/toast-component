ac408e651901ad46fdd4fc933b62b4514fa1c677
Tommy Nguyen <tonguyen@shutterstock.com>
2023-01-31 22:17:12 -0500

    Coded along exercise 6

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,4 +1,5 @@
 import React from "react";
+import useKeydown from "../../hooks/use-keydown";
 
 export const ToastContext = React.createContext();
 
@@ -17,19 +18,11 @@ function ToastProvider({ children }) {
     },
   ]);
 
-  React.useEffect(() => {
-    function handleKeyDown(event) {
-      if (event.code === "Escape") {
-        setToasts([]);
-      }
-    }
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
+  const handleEscape = React.useCallback(() => {
+    setToasts([]);
   }, []);
+
+  useKeydown('Escape', handleEscape);
 
   function createToast(message, variant) {
     const nextToasts = [
